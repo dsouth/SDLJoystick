31b9c1ca69fa42feda11f14df2cf06f735631760
Doug South <doug.south@gmail.com>
2017-05-09 20:34:12 +1000

    initial framework

--- main.c
+++ main.c
@@ -1,6 +1,77 @@
-#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT;
+
+SDL_Renderer *gRenderer;
+SDL_Window *gWindow;
+SDL_Joystick *gGameController;
+
+int init() {
+    int success;
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = 0;
+    } else {
+        gWindow = SDL_CreateWindow("SDL Joystick",
+                                   SDL_WINDOWPOS_UNDEFINED,
+                                   SDL_WINDOWPOS_UNDEFINED,
+                                   SCREEN_WIDTH,
+                                   SCREEN_HEIGHT,
+                                   SDL_WINDOW_SHOWN);
+        if (gWindow == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = 0;
+        } else {
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            if (gRenderer == NULL) {
+                printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+                success = 0;
+            } else {
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                success = 1;
+
+                int imgFlags = IMG_INIT_PNG;
+                if (!(IMG_Init(imgFlags) & imgFlags)) {
+                    printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+                    success = 0;
+                }
+
+                if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+                    printf("Warning: Linear texture filtering not enabled!\n");
+                }
+                if (SDL_NumJoysticks() < 1) {
+                    printf("Warning: No joysticks connected!\n");
+                    success = 0;
+                } else {
+                    gGameController = SDL_JoystickOpen(0);
+                    if (gGameController == NULL) {
+                        printf("Warning: unable to open game controller! SDL Error%s\n", SDL_GetError());
+                        success = 0;
+                    }
+                }
+            }
+        }
+    }
+    return success;
+}
+
+int loadMedia() {
+    int success = 1;
+    return success;
+}
+
+void loop() {
+
+}
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    if (!init()) {
+        printf("Falied to init! SDL Error: %s\n", SDL_GetError());
+    } else if (!loadMedia()) {
+        printf("Failed to load media!\n");
+    } else {
+        loop();
+    }
 }
